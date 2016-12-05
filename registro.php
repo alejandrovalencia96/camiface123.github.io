a09646c8af9a997ed6152e25224bf2d536b752b4
camiface123 <alejandrovalencia012@hotmail.com>
2016-12-05 18:58:32 -0500

    Primer Sitio Web

---
+++ registro.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+Bienvenido
+ <?php echo $_POST["nombre"]; ?><br>
+su email es: <?php echo $_POST["email"];?><br>
+Su comentario es: <?php echo $_POST["mensaje"]; ?>
+
+
+
+
+
+</body>
+</html> 
